8ad1f795d577c03b74e1061cf566e38f27f2e68c
avanichy25 <avanichy@gmail.com>
2023-06-30 15:12:58 -0700

    connection details

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -36,9 +36,12 @@ terraform {
 }
 
 
-
 # 2. Terraform Provider Block for AzureRM
 provider "azurerm" {
+  client_id= "d5c2020c-66a9-48c8-ba0c-bae44501587c"
+  client_secret= "RmR8Q~w6hoUGYwbsiUfb~2EiERi~Mda-xxu0zbWc"
+  subscription_id= "058bc466-7900-46a0-9403-eb04e3388a62"
+  tenant_id= "ad1e5f27-489b-4bad-a209-f3b5741fd81c"
   features {
 
   }

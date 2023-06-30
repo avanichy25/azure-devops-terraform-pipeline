fd7b69a4fd711e49e8ab18e9925a1adec480b34c
avanichy25 <avanichy@gmail.com>
2023-06-30 15:59:41 -0700

    added the changes for the admin

--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -1,6 +1,6 @@
 # Create Azure AD Group in Active Directory for AKS Admins
 resource "azuread_group" "azure_admin" {
-  display_name =       "${azurerm_resource_group.az-rg.name}-cluster-administrators"
+  display_name =       "${azurerm_resource_group.aks_rg.name}-cluster-administrators"
   description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.az-rg.name}-cluster."
   mail_enabled     = true
   mail_nickname    = "azure_admin"
